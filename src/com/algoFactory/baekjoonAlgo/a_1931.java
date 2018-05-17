78ccda377df4d76912a6b02406db30a261094d67
Minwoo-Kang <sheepbomb@naver.com>
2018-05-17 23:49:01 +0900

    .

--- src/com/algoFactory/baekjoonAlgo/a_1931.java
+++ src/com/algoFactory/baekjoonAlgo/a_1931.java
@@ -17,13 +17,13 @@ public class a_1931 {
             conferences[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(conferences,(conference1 , conference2) -> conference1[1]==conference2[1] ? conference1[0]-conference2[0] : conference1[1]-conference2[1]);
+        Arrays.sort(conferences,(conference1 , conference2) -> conference1[1]==conference2[1] ? conference1[0]-conference2[0] : conference1[1]-conference2[1]); //끝나는 시간대로 정렬, 만약 같으면 시작 시간으로 정렬
 
         for (int i = 0; i < N; i++) {
 
-            if (conferences[i][0] >= endTime){
+            if (conferences[i][0] >= endTime){  //현재 기준시간 보다 시작 시간이 빠를 시 해당 회의를 선택.
                 result++;
-                endTime = conferences[i][1];
+                endTime = conferences[i][1];    //회의를 선택하면 기준 시간이 선택한 회의의 종료 시점이 된다.
             }
         }
 
